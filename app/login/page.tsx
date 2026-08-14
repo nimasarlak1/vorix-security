e80d9476c9f00113e49ca0caed26a341f7786963
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-15 00:01:58 +0330

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function LoginPage() {
+export default function ورودPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
