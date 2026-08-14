e783034f8708b802de6b4071e78f14c78aee419d
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-14 17:05:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-const form = document.getElementById("contactForm");
-
-if(form){
-
-form.addEventListener("submit", function(e){
-
-e.preventDefault();
-
-let name = form.querySelector("input").value;
-let phone = form.querySelectorAll("input")[1].value;
-let text = form.querySelector("textarea").value;
+document.addEventListener("DOMContentLoaded", () => {
 
 
-let msg =
-"درخواست جدید سایت VORIX.SECURITY\n\n" +
-"نام: " + name +
-"\nشماره: " + phone +
-"\nتوضیحات: " + text;
+const cards = document.querySelectorAll(".cards div");
 
 
-window.open(
-"https://wa.me/989357781529?text=" +
-encodeURIComponent(msg),
-"_blank"
-);
+cards.forEach((card,index)=>{
+
+card.style.opacity="0";
+card.style.transform="translateY(40px)";
+
+
+setTimeout(()=>{
+
+card.style.transition="0.8s";
+card.style.opacity="1";
+card.style.transform="translateY(0)";
+
+},index*200);
 
 
 });
 
+
+
+const title = document.querySelector(".hero h2");
+
+
+let text = title.innerHTML;
+
+title.innerHTML="";
+
+
+let i=0;
+
+
+function typing(){
+
+if(i < text.length){
+
+title.innerHTML += text[i];
+
+i++;
+
+setTimeout(typing,80);
+
 }
+
+}
+
+
+typing();
+
+
 
 });
