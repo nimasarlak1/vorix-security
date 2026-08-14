da7d3cf12633b4c75843e2c79d85ff7da2ad2032
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-14 21:18:08 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Navber from "../styles/components/Navber";
+import Navbar from "../styles/components/Navbar";
 import Hero from "../styles/components/Hero";
 import Services from "../styles/components/Services";
 import About from "../styles/components/About";
@@ -6,7 +6,6 @@ import Projects from "../styles/components/Projects";
 import Testimonials from "../styles/components/Testimonials";
 import Contact from "../styles/components/Contact";
 import Footer from "../styles/components/Footer";
-
 export default function Home() {
   return (
     <main dir="rtl">
