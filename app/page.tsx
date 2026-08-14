f8f66155a3ede8aafa6a521931b867f9a303cc63
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-14 21:53:27 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,17 @@ import Projects from "../styles/components/Projects";
 import Testimonials from "../styles/components/Testimonials";
 import Contact from "../styles/components/Contact";
 import Footer from "../styles/components/Footer";
+
+export default function Home() {
+  return (
     <main dir="rtl">
       <Navbar />
-
       <Hero />
-
       <Services />
-
       <About />
-
       <Projects />
-
       <Testimonials />
-
       <Contact />
-
       <Footer />
     </main>
   );
