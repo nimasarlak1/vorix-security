d0768132c6b10ac312f426fe44d83f0bae49967a
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-15 03:46:38 +0330

    Update page.tsx

--- app/customer/page.tsx
+++ app/customer/page.tsx
@@ -1,1 +1,48 @@
+"use client";
 
+import { useEffect, useState } from "react";
+
+export default function CustomerPage() {
+
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const data = localStorage.getItem("user");
+
+    if (data) {
+      setUser(JSON.parse(data));
+    }
+  }, []);
+
+
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+
+      <div className="w-full max-w-md p-8 border rounded-xl">
+
+        <h1 className="text-2xl mb-6 text-center">
+          پنل مشتری
+        </h1>
+
+
+        {user && (
+          <div>
+            <p>
+              نام: {user.name}
+            </p>
+
+            <p>
+              ایمیل: {user.email}
+            </p>
+
+            <p>
+              نوع حساب: مشتری
+            </p>
+          </div>
+        )}
+
+      </div>
+
+    </main>
+  );
+}
