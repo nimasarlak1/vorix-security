9f7978d85b11ca0d9065ff5853de7d89099a03c6
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-15 03:43:50 +0330

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,58 +1,102 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { users } from "@/lib/users";
 
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+
+  const router = useRouter();
+
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [message,setMessage] = useState("");
+
 
   const handleLogin = () => {
-    if (!email || !password) {
-      setMessage("لطفا ایمیل و رمز عبور را وارد کنید");
+
+    const user = users.find(
+      (u)=> 
+        u.email === email &&
+        u.password === password
+    );
+
+
+    if(!user){
+      setMessage("ایمیل یا رمز عبور اشتباه است");
       return;
     }
 
-    setMessage("ورود انجام شد");
+
+    localStorage.setItem(
+      "user",
+      JSON.stringify(user)
+    );
+
+
+    if(user.role === "admin"){
+
+      router.push("/dashboard");
+
+    }else{
+
+      router.push("/customer");
+
+    }
+
   };
 
+
   return (
+
     <main className="min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md p-6">
-        <h1 className="text-3xl mb-6">
+
+      <div className="w-full max-w-md p-8 border rounded-xl">
+
+
+        <h1 className="text-2xl mb-6 text-center">
           ورود
         </h1>
 
+
         <input
-          className="border p-2 w-full mb-3"
-          type="email"
+          className="border p-3 w-full mb-3"
           placeholder="ایمیل"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e)=>setEmail(e.target.value)}
         />
+
 
         <input
-          className="border p-2 w-full mb-3"
-          type="password"
+          className="border p-3 w-full mb-3"
           placeholder="رمز عبور"
+          type="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e)=>setPassword(e.target.value)}
         />
 
+
         <button
-          className="border px-4 py-2"
           onClick={handleLogin}
+          className="bg-black text-white w-full p-3 rounded"
         >
           ورود
         </button>
 
-        {message && (
+
+        {
+          message &&
           <p className="mt-4">
             {message}
           </p>
-        )}
+        }
+
+
       </div>
+
     </main>
+
   );
+
 }
