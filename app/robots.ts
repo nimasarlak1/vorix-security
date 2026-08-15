3c1f60054e95659545a7e6e069c3e3c152548cea
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-15 09:00:18 +0330

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,15 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://vorix-security.nimasarlak380.workers.dev';
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/dashboard/', '/admin/login'],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
 
