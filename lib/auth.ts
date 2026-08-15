41b6cf2b4872b55fb8761380b57815b70172bd4b
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-15 03:38:49 +0330

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,18 @@
+import { users } from "./users";
 
+
+export function loginUser(
+ email:string,
+ password:string
+){
+
+ const user = users.find(
+  u =>
+  u.email === email &&
+  u.password === password
+ );
+
+
+ return user || null;
+
+}
