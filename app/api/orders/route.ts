efc5c56fd1c5915d225a8f59627a35250be3a197
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-17 01:06:32 +0330

    Update route.ts

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic'; 
-export conts runtime = 'edge';
+export const dynamic = 'force-dynamic'; export const runtime = 'edge';
 
 export async function POST(request: Request) {
   try {
