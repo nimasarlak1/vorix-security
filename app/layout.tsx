f7de6089a7cf47d9704ccb66e410db279b5f2d32
nimasarlak1 <nimasarlak380@gmail.com>
2026-08-18 20:27:39 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,6 @@ import Footer from './components/Footer';
 export const metadata = {
   title: 'VORIX.SECURITY',
   description: 'امنیت دیجیتال و راهکارهای هوش مصنوعی',
-  verification: {
-    google: 'eWpLO4qdD5WZe49-VnjdBy2vx3uMrlMVBFnQBx7NvVk',
-  },
 };
 
 export default function RootLayout({
@@ -17,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir="rtl">
+      <head>
+        <meta name="google-site-verification" content="eWpLO4qdD5WZe49-VnjdBy2vx3uMrlMVBFnQBx7NvVk" />
+      </head>
       <body className="bg-neutral-950 text-white selection:bg-cyan-500 selection:text-black min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow">
